d8b29b873161b2abbf74535b6021b730cd7b239e
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-05-17 09:27:24 -0700

    ngs start codes

---
+++ ngs-proje/src/App.jsx
@@ -0,0 +1,11 @@
+import Routing from './Routing'
+import "./assets/styles/main.css";
+function App() {
+  return (
+    <>
+<Routing/>
+    </>
+  )
+}
+
+export default App
---
+++ ngs-proje/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+const Home = () => {
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default Home
