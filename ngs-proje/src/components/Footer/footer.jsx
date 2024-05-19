a195a27b5c09297846ed1042d40f3f1f3c60b1fa
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-05-19 00:51:15 -0700

    loading proje

--- ngs-proje/src/components/Footer/footer.jsx
+++ ngs-proje/src/components/Footer/footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import Container from 'react-bootstrap/esm/Container'
+import logo from "../../assets/images/logo.png"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div id="footer">
+        <Container>
+           <div className="footer__top">
+            <div className="footer__left">
+<div className="footer__logo">
+    <img src={logo} alt="" />
+</div>
+<div className="footers__lefttext">
+Təhlükəsizliyin yeni nəsli!
+</div>
+            </div>
+            <div className="footer__center"></div>
+            <div className="footer__right">
+
+            </div>
+           </div>
+        </Container>
+    </div>
   )
 }
 
