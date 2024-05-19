3bb5e98da87c182913d0b5a9eb4e1492793089f4
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-05-19 00:25:50 -0700

    start codes

--- ngs-proje/src/pages/Home/index.jsx
+++ ngs-proje/src/pages/Home/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import PageContainer from '../../components/PageContainer';
 const Home = () => {
   return (
     <>
- 
+ <PageContainer>
+  
+ </PageContainer>
     </>
   )
 }
