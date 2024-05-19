bfbbd26d7aa5e1d4a0d01e0da4e7936c068fc1d5
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-05-19 09:20:11 -0700

    footer finis

--- ngs-proje/src/components/Footer/footer.jsx
+++ ngs-proje/src/components/Footer/footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import logo from "../../assets/images/logo.png"
+import facebook from "../../assets/images/footericon/facebook.png"
+import instagram from "../../assets/images/footericon/instagram.png"
+import linkedn from "../../assets/images/footericon/linkedn.png"
+import email from "../../assets/images/footericon/email.png"
+import phone from "../../assets/images/footericon/phone.png"
+
 const Footer = () => {
+   
   return (
     <div id="footer">
         <Container>
-           <div className="footer__top">
+           <div className="footer__container">
             <div className="footer__left">
 <div className="footer__logo">
     <img src={logo} alt="" />
@@ -13,13 +20,65 @@ const Footer = () => {
 <div className="footers__lefttext">
 Təhlükəsizliyin yeni nəsli!
 </div>
+<div className="footer__icon">
+    <a href="">
+    <img src={facebook} alt="" />
+    </a>
+    <a href="">
+    <img src={instagram} alt="" />
+    </a>
+    <a href="">
+    <img src={linkedn} alt="" />
+    </a>
+</div>
             </div>
-            <div className="footer__center"></div>
+            <div className="footer__center">
+                <div className="footer__link">
+                    <a className='linkcolor' href="">Ana Səhifə</a>
+                    <a className='linkcolor' href="">Haqqımızda</a>
+                    <a className='linkcolor' href="">Xidmətlər</a>
+                    <a className='linkcolor' href="">Xəbərlər</a>
+                    <a className='linkcolor' href="">Bloq</a>
+                    <a className='linkcolor' href="">Təlimlər</a>
+                </div>
+            </div>
             <div className="footer__right">
+<div className="footer__contact">
 
+    <p className='contacttext'>Əlaqə</p>
+    <div className="footer__contacticon">
+
+    <div className="footer__mail">
+        <img src={email} alt="" />
+        <p>ngs.azerbaijan@gmail.com</p>
+    </div>
+    <div className="footer__phone">
+        <img src={phone} alt="" />
+        <p>+994 51 316 88 94</p>
+    </div>
+    </div>
+</div>
+<p className='footer__companyname'>© 2024 Sayt Binary School tərəfindən hazırlanmışdır.</p>
             </div>
+
+
+
+
+
            </div>
         </Container>
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
--- ngs-proje/src/components/PageContainer/index.jsx
+++ ngs-proje/src/components/PageContainer/index.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
 import Header from '../Header/header'
 import Footer from '../Footer/footer'
-
+import  { useState, useEffect } from 'react';
 const PageContainer = ({children}) => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
   return (
    <>
    <Header/>
    {children}
+   
+   <div className="App">
+      <div style={{ height: '1200px' }}> 
+      </div>
+      {showButton && (
+        <button onClick={scrollToTop} className="scroll-to-top">
+          ⬆️
+        </button>
+      )}
+    </div>
    <Footer/>
    </>
   )
