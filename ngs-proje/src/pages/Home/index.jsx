abab29abbbd07f0da9ba8f84f6b939a555e46bc2
Eyyubfront <eyyubbayramov58@gmail.com>
2024-05-21 18:02:10 +0400

    swiper done

--- ngs-proje/src/pages/Home/index.jsx
+++ ngs-proje/src/pages/Home/index.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import PageContainer from '../../components/PageContainer';
-import Certifaciton from '../../components/Certifaciton/cerfitacion';
+import React from "react";
+import PageContainer from "../../components/PageContainer";
+import Certifaciton from "../../components/Certifaciton/cerfitacion";
+import Banner from "../../components/Banner/Banner";
 const Home = () => {
   return (
     <>
- <PageContainer>
-  
-  <Certifaciton/>
- </PageContainer>
+      <PageContainer>
+        <Banner />
+        <Certifaciton />
+      </PageContainer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
