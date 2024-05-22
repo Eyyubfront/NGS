c62f4387f8792c321d3042a8f6c015dfa32264b7
Eyyubfront <eyyubbayramov58@gmail.com>
2024-05-22 17:19:08 +0400

    task done

---
+++ ngs-proje/src/components/Partners/Partners.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import group from "../../assets/images/Group.png";
+import hilay from "../../assets/images/hilayqara.png";
+import binary from "../../assets/images/Logo.png";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+
+const Partners = () => {
+  return (
+    <div id="partners">
+      <h2>Tərəfdaşlar</h2>
+
+      <Swiper
+        slidesPerView={6}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 6,
+            },
+            1444: {
+              slidesPerView: 6,
+            },
+          }}
+      >
+        <SwiperSlide>
+          <img src={group} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={binary} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={hilay} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={group} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={binary} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={hilay} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={group} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={binary} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={hilay} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+      </Swiper>
+
+      {/* <Swiper
+        slidesPerView={6}
+        loop={true}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+          1444: {
+            slidesPerView: 6,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <img src={group} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={binary} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={hilay} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={group} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={binary} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={hilay} className="slide__img" alt="swiper" />
+        </SwiperSlide>
+      </Swiper> */}
+    </div>
+  );
+};
+
+export default Partners;
---
+++ ngs-proje/src/components/Training/Training.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Training = () => {
+  return (
+    <div id="training">
+      <div className="training__content">
+        <h2>Təlimlər, tədbir və elanlar</h2>
+        <p>
+          Təlimlərimizdən xəbərdar olmaq, tədbirlərimizdə iştirak etmək üçün
+          səhifəyə keçid edin. Qeydiyyatdan keçərək bizə qoşula və komandamızın
+          bir parçasına çevrilə bilərsiniz.
+        </p>
+        <button>Ətraflı</button>
+      </div>
+    </div>
+  );
+};
+
+export default Training;
