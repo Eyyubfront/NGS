1f97d64eec6c13d478e658949d35a0d856850ff5
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-09-08 05:35:04 -0700

    finish

--- ngs-proje/src/components/Footer/footer.jsx
+++ ngs-proje/src/components/Footer/footer.jsx
@@ -7,6 +7,7 @@ import linkedin from "../../assets/images/footericon/linkedn.png";
 import email from "../../assets/images/footericon/email.png";
 import phone from "../../assets/images/footericon/phone.png";
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom'; // Link'i import edin
 
 const Footer = () => {
     const { t, i18n } = useTranslation();
@@ -22,28 +23,28 @@ const Footer = () => {
                     <div className="footer__top">
                         <div className="footer__left">
                             <div className="footer__logo">
-                                <img src={logo} alt="" />
+                                <img src={logo} alt="Logo" />
                             </div>
                         </div>
                         <div className="footer__center">
                             <div className="footer__link">
-                                <a className='linkcolor' href="">{t('Home')}</a>
-                                <a className='linkcolor' href="">{t('aboutUs')}</a>
-                                <a className='linkcolor' href="">{t('services')}</a>
-                                <a className='linkcolor' href="">{t('news')}</a>
-                                <a className='linkcolor' href="">{t('blog')}</a>
-                                <a className='linkcolor' href="">{t('trainings')}</a>
+                                <Link className='linkcolor' to="/">{t('Home')}</Link>
+                                <Link className='linkcolor' to="/aboutus">{t('aboutUs')}</Link>
+                                <Link className='linkcolor' to="/servicesection">{t('services')}</Link>
+                                <Link className='linkcolor' to="/news">{t('news')}</Link>
+                                <Link className='linkcolor' to="/blogs">{t('blog')}</Link>
+                                <Link className='linkcolor' to="/traningpages">{t('trainings')}</Link>
                             </div>
                         </div>
                         <div className="footer__right">
                             <div className="footer__contact">
                                 <div className="footer__contacticon">
                                     <div className="footer__mail">
-                                        <img src={email} alt="" />
+                                        <img src={email} alt="Email Icon" />
                                         <p>ngs.azerbaijan@gmail.com</p>
                                     </div>
                                     <div className="footer__phone">
-                                        <img src={phone} alt="" />
+                                        <img src={phone} alt="Phone Icon" />
                                         <p>+994 51 316 88 94</p>
                                     </div>
                                 </div>
@@ -52,14 +53,14 @@ const Footer = () => {
                     </div>
                     <div className="footer__bottom">
                         <div className="footer__icon">
-                            <a href="">
-                                <img src={facebook} alt="" />
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                <img src={facebook} alt="Facebook" />
                             </a>
-                            <a href="">
-                                <img src={instagram} alt="" />
+                            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                                <img src={instagram} alt="Instagram" />
                             </a>
-                            <a href="">
-                                <img src={linkedin} alt="" />
+                            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                                <img src={linkedin} alt="LinkedIn" />
                             </a>
                         </div>
                     </div>
